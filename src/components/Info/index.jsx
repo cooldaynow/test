982f00e49ed0ac8725d288a76ba29d1864b04d67
coolday <cooldaynow@gmail.com>
2019-08-06 18:42:58 +0300

    add task

--- src/components/Info/index.jsx
+++ src/components/Info/index.jsx
@@ -1,46 +1,15 @@
 import React from 'react';
-import {
-  Row,
-  Col,
-  ListGroup,
-  ListGroupItem,
-  ListGroupItemHeading,
-  ListGroupItemText,
-} from 'reactstrap';
+import {Row, Col} from 'reactstrap';
+import Task from '../Task';
 import './index.scss';
-import ListItem from '../ListItem';
-import data from '../../api/text.js';
 
 const Info = () => {
   return (
     <Row className="info__wrap">
       <Col className="info__col">
-        <h2>Тестовое задание "Разговорная лексика" </h2>
+        <h2>"Разговорная лексика" </h2>
         <Row className="info__body">
-          <Col className="description">
-            {data.description.map(elem => <div>
-            <ul>
-              <li>
-                {elem.text}
-              </li>
-              <ul>
-                <li>
-                  {elem.subText}
-                </li>
-              </ul>
-            </ul>
-          </div>
-            )}
-          </Col>
-          <Col className="implementation">
-            <ListGroup>
-              <ListGroupItem>Cras justo odio</ListGroupItem>
-              <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-              <ListGroupItem>Morbi leo risus</ListGroupItem>
-              <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-              <ListGroupItem>Vestibulum at eros</ListGroupItem>
-            </ListGroup>
-          </Col>
+          <Task />
         </Row>
       </Col>
     </Row>
---
+++ src/components/Task/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './index.scss';
+import data from '../../api/text.js';
+import {random} from '../../utils';
+
+const Task = () => (
+  <div className="task__wrap">
+    <h4>Задание</h4>
+    <ul>
+      {data.map((paragraph, i) => (
+        <li className="task__paragraph" key={random(0, 999) * i}>
+          {paragraph.text}
+          {paragraph.hasOwnProperty('subText')
+            ? paragraph.subText.map(subText => (
+                <ul key={random(0, 999) * i}>
+                  <li>{subText}</li>
+                </ul>
+              ))
+            : null}
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default Task;
