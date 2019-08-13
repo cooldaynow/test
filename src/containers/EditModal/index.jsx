7aa1c0e02e124b507e136dc09bba893c03a22d4b
cooldaynow <cooldaynow@gmail.com>
2019-08-13 16:52:47 +0300

    remove css

--- src/containers/EditModal/index.jsx
+++ src/containers/EditModal/index.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import {Modal, ModalHeader, ModalBody} from 'reactstrap';
 import EditContents from '../EditContents/';
 import AddButton from '../../components/AddButton';
-//import './index.scss';
 
 const EditModal = ({col, change}) => {
   const [isOpen, setIsOpen] = useState(false);
